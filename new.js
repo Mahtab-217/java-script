6d5df3449de26dac2b437b8bdda7b6631ac26372
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-11-19 21:58:07 +0430

    functions in js

--- new.js
+++ new.js
@@ -1,9 +1,13 @@
-let i=0;
-while(i<9){
-    console.log("Hi There! How are you doing?");
-    i++
+function sum(num1, num2, num3, num4){
+    console.log(num1*num2+num3+num4)
 }
-do{
-    console.log("Hello World");
-    i++
-}while(i<3);
+sum(7,4,4,3);
+sum(78,21,89,38);
+sum(2,4,54,908,1000);
+
+
+function multiply(num1,num2,num3){
+    console.log(num1*num2*num3)
+}
+multiply(4,90,3);
+multiply(1908140,457880,6777868787834)
