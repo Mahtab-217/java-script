1a37d7f765cd761fd2c326a8fa2790a968bc2113
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-11-22 21:21:17 +0430

    working with template leterals

--- new.js
+++ new.js
@@ -14,3 +14,21 @@ return "Hello Dear "+name;
 }
 const Mah= greeting("Mahtab");
 const Hasti = greeting("Hasti");
+
+const name="Taqi";
+const score =70;
+const course ="Sharifi Center"
+const result =score>50? ` Dear ${name}  Your score is ${score} and you successfully passed the ${course} exam's`: "You failed";
+
+const blood =120;
+if (blood>50){
+    console.log("your blood presure is low")
+}
+else if(blood<100){
+    console.log("your blood presure is high")
+}
+else{
+    console.log("You don't have any problem")
+}
+
+ 
