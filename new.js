6d0d777bfea921fe1b770149203ef9f1d94bf2a7
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-11-24 21:58:31 +0430

    js works

--- new.js
+++ new.js
@@ -1,34 +1,8 @@
-const h1 =document.getElementById("h1");
-const btn = document.getElementById("btn");
-const btn2 =document.getElementById("btn2");
-
-btn.addEventListener("click", ()=>{
-    h1.innerHTML = "Your Data Is Saved ";
-})
-btn2.addEventListener("click", ()=>{
-    h1.innerHTML="";
-})
-
-const greeting = function(name){
-return "Hello Dear "+name;
+const name="Hasti";
+const result=name==="zainab"?"Hi dear Zainab you are my sister":name==="Ali"?"Hi dear Ali you are my brother":name==="Hasti"?"Hi dear Hasti you are my cousin":"I don't know you";
+const number=10;
+const result2=number<=20 && number>=15?"You are in gruoup A":number>=7?"You are in group B":"You are in group C";
+const greeting = (name,lastName)=>{
+console.log("Hi there dear "+name +" "+lastName);
 }
-const Mah= greeting("Mahtab");
-const Hasti = greeting("Hasti");
-
-const name="Taqi";
-const score =70;
-const course ="Sharifi Center"
-const result =score>50? ` Dear ${name}  Your score is ${score} and you successfully passed the ${course} exam's`: "You failed";
-
-const blood =120;
-if (blood>50){
-    console.log("your blood presure is low")
-}
-else if(blood<100){
-    console.log("your blood presure is high")
-}
-else{
-    console.log("You don't have any problem")
-}
-
- 
+greeting("Gandom","Raha");
