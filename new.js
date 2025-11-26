18dc8d4852845b4c7f8b74e3544340957df66135
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-11-26 21:15:53 +0430

    functions

--- new.js
+++ new.js
@@ -6,3 +6,36 @@ const greeting = (name,lastName)=>{
 console.log("Hi there dear "+name +" "+lastName);
 }
 greeting("Gandom","Raha");
+
+const Hi =(name,lastName) => ` Welcome dear ${name}${lastName}`;
+const Hello =Hi(" Sama ", " Alisada");
+
+const Say=(a,b="Frishta")=>`Your father name is ${a} and your own name is ${b}`;
+const say= Say("Asghar");
+
+// const score=(...a)=>{
+//     let total =0;
+//     for(let i=0; i<a.length;i++){
+//         total=total+a[i];
+//     }
+//     console.log();
+
+// }
+// score(3,4,3,4,5,7)
+const sum=(...a)=>{
+      let total =0;
+   for(let i=0; i<a.length;i++){
+       total=total+a[i];
+   }
+    console.log(total);
+}
+sum(323,23,15,65,76,8,90,880)
+
+const plus =(...b)=>{
+    let all =0;
+    for(let n=0; n<b.length;n++){
+        all=all-b[n];
+    }
+    console.log(all);
+}
+plus(3,4,5,50,90,99,88,77.89)
